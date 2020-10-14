8b705f829a74c84cc29e9e4e94adf4c8e4aa936c
PaolaDM <orchidea.preziosa@gmail.com>
2020-10-14 22:33:06 +0100

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import App from './App';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+Enzyme.configure({ adapter: new Adapter() });
+
+// // test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+// shallow does a fake render(it will call but doesn't modify the DOM as in our case we don't have one) of a component.
+
+describe('App', () => {
+  it('Runs the tests', () => {
+    const wrapper = shallow(<App />);
+    console.log(wrapper.state());
+    expect(true).toEqual(true);
+
+  });
 });
+
+
